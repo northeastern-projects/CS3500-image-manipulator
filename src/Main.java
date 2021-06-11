39489b739e80f0f6f9806dc3404579ae60895e9c
Rahul Rao <rahulrao@email.com>
2021-06-11 12:30:56 +0800

    Its complete, it works and its fast :)

--- src/Main.java
+++ src/Main.java
@@ -15,36 +15,37 @@ public class Main {
   //demo main
   public static void main(String []args) throws IOException {
     String filename;
+    String name = "road";
 
     if (args.length > 0) {
       filename = args[0];
     }
     else {
-      filename = "res/check.ppm";
+      filename = "res/" + name + ".ppm";
     }
 
-    img = new Checkerboard(19, 11, 255);
-    img.save("check");
+//    img = new Checkerboard(19, 11, 255);
+//    img.save("check");
 
-//    img = ImageUtil.readPPM(filename);
-//    IModifier mod = new Blur();
-//    img.applyFilter(mod);
-//    img.save("flower" + "_" + mod);
-//
-//    img = ImageUtil.readPPM(filename);
-//    mod = new Sharpen();
-//    img.applyFilter(mod);
-//    img.save("flower" + "_" + mod);
-//
-//    img = ImageUtil.readPPM(filename);
-//    mod = new Sepia();
-//    img.applyFilter(mod);
-//    img.save("flower" + "_" + mod);
-//
-//    img = ImageUtil.readPPM(filename);
-//    mod = new Greyscale();
-//    img.applyFilter(mod);
-//    img.save("flower" + "_" + mod);
+    img = ImageUtil.readPPM(filename);
+    IModifier mod = new Blur();
+    img.applyFilter(mod);
+    img.save(name + "_" + mod);
+
+    img = ImageUtil.readPPM(filename);
+    mod = new Sharpen();
+    img.applyFilter(mod);
+    img.save(name + "_" + mod);
+
+    img = ImageUtil.readPPM(filename);
+    mod = new Sepia();
+    img.applyFilter(mod);
+    img.save(name + "_" + mod);
+
+    img = ImageUtil.readPPM(filename);
+    mod = new Greyscale();
+    img.applyFilter(mod);
+    img.save(name + "_" + mod);
   }
 
 }
