59e3fb382c89a625c1fb85878a217ba2c6b28c03
Rahul Rao <rahulrao@email.com>
2021-06-27 22:55:27 +0800

    Fixed errors and better UX

--- src/Main.java
+++ src/Main.java
@@ -22,43 +22,43 @@ public class Main {
   /**
    * Demo of main with two text and one gui view.
    */
-//  public static void main(String[] args) throws IOException, UnsupportedLookAndFeelException {
-//    ITextView view;
-//    IGraphicalView view2;
-//    IController controller;
-//    ILayer model = new Layer();
-//    if (args.length < 1) {
-//      throw new IllegalArgumentException("Please enter 'file' followed by the txt file name for"
-//              + "file-based scripting or enter 'interactive' for user input.\n");
-//    }
-//    if (args[0].equalsIgnoreCase("-script")) {
-//      view = new TextView(new FileReader(args[1]), System.out);
-//      controller = new TextController(view, model);
-//      controller.go();
-//    } else if (args[0].equalsIgnoreCase("-text")) {
-//      view = new TextView(new InputStreamReader(System.in), System.out);
-//      controller = new TextController(view, model);
-//      controller.go();
-//    } else if (args[0].equalsIgnoreCase("-interactive")) {
-//      UIManager.setLookAndFeel(new NimbusLookAndFeel());
-//      view2 = new GraphicalView("Image manipulator!", model);
-//      controller = new GraphicalController(view2, model);
-//      controller.go();
-//    } else {
-//      throw new IllegalArgumentException("Please enter 'file' followed by the txt file name for"
-//              + "file-based scripting or enter 'interactive' for user input.\n");
-//    }
-//  }
-
-  public static void main(String[] args) throws UnsupportedLookAndFeelException, IOException {
+  public static void main(String[] args) throws IOException, UnsupportedLookAndFeelException {
     ITextView view;
     IGraphicalView view2;
     IController controller;
     ILayer model = new Layer();
-
-    UIManager.setLookAndFeel(new NimbusLookAndFeel());
-    view2 = new GraphicalView("Image manipulator!", model);
-    controller = new GraphicalController(view2, model);
-    controller.go();
+    if (args.length < 1) {
+      throw new IllegalArgumentException("Please enter 'file' followed by the txt file name for"
+              + "file-based scripting or enter 'interactive' for user input.\n");
+    }
+    if (args[0].equalsIgnoreCase("-script")) {
+      view = new TextView(new FileReader(args[1]), System.out);
+      controller = new TextController(view, model);
+      controller.go();
+    } else if (args[0].equalsIgnoreCase("-text")) {
+      view = new TextView(new InputStreamReader(System.in), System.out);
+      controller = new TextController(view, model);
+      controller.go();
+    } else if (args[0].equalsIgnoreCase("-interactive")) {
+      UIManager.setLookAndFeel(new NimbusLookAndFeel());
+      view2 = new GraphicalView("Image manipulator!", model);
+      controller = new GraphicalController(view2, model);
+      controller.go();
+    } else {
+      throw new IllegalArgumentException("Please enter 'file' followed by the txt file name for"
+              + "file-based scripting or enter 'interactive' for user input.\n");
+    }
   }
+
+//  public static void main(String[] args) throws UnsupportedLookAndFeelException, IOException {
+//    ITextView view;
+//    IGraphicalView view2;
+//    IController controller;
+//    ILayer model = new Layer();
+//
+//    UIManager.setLookAndFeel(new NimbusLookAndFeel());
+//    view2 = new GraphicalView("Image manipulator!", model);
+//    controller = new GraphicalController(view2, model);
+//    controller.go();
+//  }
 }
